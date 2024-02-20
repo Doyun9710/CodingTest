12abff3b0879694ecb9291efd7d3c6c584d46bbb
Doyun Lee <doyun1010k@gmail.com>
2024-02-20 22:05:23 +0900

    20240220

--- 이것이 코딩 테스트다/4_1_1.py
+++ 이것이 코딩 테스트다/4_1_1.py
@@ -3,17 +3,27 @@
 import sys
 input = sys.stdin.readline
 
-N = input()
+N = int(input())
 movelist = list(map(str, input().split()))
 
 x, y = 1, 1
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-move_type = ['L', 'R', 'U', 'D']
+move_types = ['L', 'R', 'U', 'D']
 
 for i in movelist:
-    # print(i)
-    pass
+    for j in range(len(move_types)):
+        if i == move_types[j]:
+            nx = x + dx[j]
+            ny = y + dy[j]
+
+    if nx < 1 or ny < 1 or nx > N or ny > N:
+        continue
+
+    x, y = nx, ny
+
+print(x, y)
+
 
 
 
